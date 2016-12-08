d916befd45b2f2e7ecdca40c5d171da13682867d
wjones20 <wjones20@emich.edu>
2016-12-08 11:45:56 -0500

    Delete mods.bat

--- CDV 0.7.0/mods.bat
+++
@@ -1,3 +0,0 @@
-cd "C:\Program Files\nodejs" 
-npm install filesystem && npm install https && npm install seriate && npm install request && npm install querystring && npm install dateformat && npm install crypto && npm install url
-pause

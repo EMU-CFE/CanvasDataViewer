4af8459764d33c6d12d3eac2eacccca03a1cb23d
wjones20 <wjones20@emich.edu>
2016-12-08 11:37:28 -0500

    Add files via upload

---
+++ AutoConfig.ps1
@@ -0,0 +1,30 @@
+$configdir=$args[0]
+$APIK=$args[1]
+$APIS=$args[2]
+$cdsvr=$args[3]
+$cdusr=$args[4]
+$cdpw=$args[5]
+
+$configfiles = Get-ChildItem $configdir\*.cdconfig -rec
+foreach ($file in $configfiles)
+{
+  (get-content $file) |
+  foreach-object {$_ -replace "IRKEYREPLACEME", $APIK} | 
+  foreach-object {$_ -replace "IRSECRETREPLACEME", $APIS} |
+  foreach-object {$_ -replace "IRCDSVRREPLACEME", $cdsvr} | 
+  foreach-object {$_ -replace "IRCDSVRUSRREPLACEME", $cdusr} | 
+  foreach-object {$_ -replace "IRCDSVRUSRPWREPLACEME", $cdpw} | 
+  set-content $file
+}
+
+New-Item -type directory -path C:\CanvasData
+$Acl = Get-Acl "C:\CanvasData"
+$Ar = New-Object  system.security.accesscontrol.filesystemaccessrule("NT SERVICE\MSSQLSERVER","FullControl","Allow")
+$Acl.SetAccessRule($Ar)
+Set-Acl "C:\CanvasData" $Acl
+
+New-Item -type directory -path C:\CanvasDataFiles
+$Acl = Get-Acl "C:\CanvasDataFiles"
+$Ar = New-Object  system.security.accesscontrol.filesystemaccessrule("NT SERVICE\MSSQLSERVER","FullControl","Allow")
+$Acl.SetAccessRule($Ar)
+Set-Acl "C:\CanvasDataFiles" $Acl
---
+++ DownloadLatestSchemaAndTables.bat
@@ -0,0 +1,3 @@
+cd "C:\Program Files\nodejs" 
+node CanvasData_Schema_Latest.js
+node CanvasData_Tables_Latest.js
